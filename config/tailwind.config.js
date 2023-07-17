e796f9916b6631d828090212109e7c81308004db
proteinreport <contact@proteinreport.org>
2023-07-17 21:50:32 +0200

    remove aspect ratio module config

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -3,11 +3,7 @@
 module.exports = {
   darkMode: 'class',
 	content: ['./hugo_stats.json'],
-  corePlugins: {
-    aspectRatio: false,
-  },
   plugins: [
-    require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
   ],
