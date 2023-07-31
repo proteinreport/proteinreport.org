38b1132d6007d1e55cfb949d0a757c74607c06e1
proteinreport <contact@proteinreport.org>
2023-07-31 18:17:47 +0200

    navbar

--- themes/pr/assets/js/app.js
+++ themes/pr/assets/js/app.js
@@ -47,3 +47,15 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 });
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.scrollY;
+window.onscroll = function() {
+  var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-200px";
+  }
+  prevScrollpos = currentScrollPos;
+}
