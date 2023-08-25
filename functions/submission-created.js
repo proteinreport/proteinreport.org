4838013375dee93d752add2e827a5c89528b1ef1
proteinreport <contact@proteinreport.org>
2023-08-25 20:45:27 +0200

    mj function testing

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,16 +1,49 @@
-const mailjet = require ('node-mailjet')
-	.connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE)
-const request = mailjet
-	.post("contact", {'version': 'v3'})
-	.request({
-      "IsExcludedFromCampaigns":"true",
-      "Name":"New Contact",
-      "Email":"passenger@mailjet.com"
-    })
-request
-	.then((result) => {
-		console.log(result.body)
-	})
-	.catch((err) => {
-		console.log(err.statusCode)
-	})
+const axios = require('axios');
+
+exports.handler = async function(event, context) {
+  try {
+    if (event.httpMethod !== 'POST') {
+      return {
+        statusCode: 405,
+        body: JSON.stringify({ message: 'Method Not Allowed' })
+      };
+    }
+
+    const { email } = JSON.parse(event.body);
+
+    if (!email) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Missing email in request body' })
+      };
+    }
+
+    const payload = {
+      Email: email,
+      ContactsLists: [process.env.MAILJET_LIST_ID],
+    };
+
+    const response = await axios.post(
+      'https://api.mailjet.com/v3/REST/contact',
+      payload,
+      {
+        auth: {
+          username: process.env.MJ_APIKEY_PUBLIC,
+          password: process.env.MJ_APIKEY_PRIVATE
+        }
+      }
+    );
+
+    return {
+      statusCode: response.status,
+      body: JSON.stringify({ message: 'Subscriber added successfully' })
+    };
+  } catch (error) {
+    console.error('Error:', error);
+
+    return {
+      statusCode: error.response ? error.response.status : 500,
+      body: JSON.stringify({ message: 'An error occurred' })
+    };
+  }
+};
