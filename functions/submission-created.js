271c2ceb47cd55aedcd4e51799f8abe0f7877aca
proteinreport <contact@proteinreport.org>
2023-08-25 16:41:23 +0200

    mailjet function testing

---
+++ functions/submission-created.js
@@ -0,0 +1,18 @@
+
+
+const mailjet = require ('node-mailjet')
+	.connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE)
+const request = mailjet
+	.post("contact", {'version': 'v3'})
+	.request({
+      "IsExcludedFromCampaigns":"true",
+      "Name":"New Contact",
+      "Email":"passenger@mailjet.com"
+    })
+request
+	.then((result) => {
+		console.log(result.body)
+	})
+	.catch((err) => {
+		console.log(err.statusCode)
+	})
