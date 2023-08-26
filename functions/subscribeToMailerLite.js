796927242ec7b4f72c496e4b332fffb6f0ca0bb9
proteinreport <contact@proteinreport.org>
2023-08-26 19:27:20 +0200

    testing result messages

--- functions/subscribeToMailerLite.js
+++ functions/subscribeToMailerLite.js
@@ -12,7 +12,6 @@ exports.handler = async (event) => {
   // Get environment variables
   const { ML_APIKEY, ML_GROUPID } = process.env;
 
-  // Create the contact in Mailjet
   try {
     const response = await axios.post(
       "https://connect.mailerlite.com/api/subscribers",
@@ -27,12 +26,29 @@ exports.handler = async (event) => {
         },
       }
     );
-    return {
-      statusCode: 201,
-      body: JSON.stringify({
-        message: "Contact added to MailerLite successfully",
-      }),
-    };
+    
+    if (response.status === 200 || response.status === 201) {
+      let message;
+      if (response.status === 200) {
+        message = "Contact added to MailerLite successfully (Status Code: 200)";
+      } else {
+        message = "Contact added to MailerLite successfully (Status Code: 201)";
+      }
+      
+      return {
+        statusCode: response.status,
+        body: JSON.stringify({
+          message: message,
+        }),
+      };
+    } else {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: "Failed to add contact to MailerLite",
+        }),
+      };
+    }
   } catch (error) {
     console.error("Error adding contact to MailerLite:", error);
 
