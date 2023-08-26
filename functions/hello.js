425e4e93e130b037a902f453b3017ddc756bc0ff
proteinreport <contact@proteinreport.org>
2023-08-26 15:57:38 +0200

    troubleshooting

--- functions/hello.js
+++ functions/hello.js
@@ -1,9 +1,9 @@
 exports.handler = async function (event, context) {
-    // Get environment variables
+  // Get environment variables
   const { MJ_APIKEY_PUBLIC, MJ_APIKEY_PRIVATE, MJ_LIST_ID } = process.env;
-  const { email } = JSON.parse(event.body);
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: email }),
+    body: JSON.stringify(event),
   };
 };
