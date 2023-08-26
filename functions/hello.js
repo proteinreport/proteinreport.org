51559f0edea482ddacfef8b7c8a680d935b8ebe8
proteinreport <contact@proteinreport.org>
2023-08-26 15:14:41 +0200

    hello testing

--- functions/hello.js
+++ functions/hello.js
@@ -1,4 +1,7 @@
 exports.handler = async function (event, context) {
+    // Get environment variables
+  const { MJ_APIKEY_PUBLIC, MJ_APIKEY_PRIVATE, MJ_LIST_ID } = process.env;
+  console.log(MJ_LIST_ID);
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello World" }),
