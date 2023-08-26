b21ea713b31420fd7da27648a93676ee0e3ad7d7
proteinreport <contact@proteinreport.org>
2023-08-26 07:57:04 +0200

    try new mj code

--- functions/subscribeToMailjet.js
+++ functions/subscribeToMailjet.js
@@ -1,33 +1,29 @@
-// Import the required dependencies
-const mailjet = require('node-mailjet').connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE);
+// netlify/functions/subscribeToMailjet.js
 
-// Define the Netlify function
+const mailjet = require('node-mailjet').connect(
+  process.env.MJ_APIKEY_PUBLIC,
+  process.env.MJ_APIKEY_PRIVATE
+);
+
 exports.handler = async (event, context) => {
   try {
-    // Parse the form data from the event body
-    const formData = JSON.parse(event.body);
+    const { email } = JSON.parse(event.body);
 
-    // Extract the email address from the form data
-    const email = formData.newsletter;
+    // Add email to Mailjet contacts
+    const result = await mailjet.post('contact').request({
+      Email: email
+    });
 
-    // Create the Mailjet contact object
-    const contact = {
-      Email: email,
-    };
-
-    // Add the contact to Mailjet using the Mailjet API
-    const result = await mailjet.post('contact').request({ Email: email });
-
-    // Return a success response
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Email added to Mailjet contacts successfully' }),
+      body: JSON.stringify({ message: 'Email added to Mailjet contacts' })
     };
   } catch (error) {
-    // Return an error response
+    console.error(error);
+
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: 'Failed to add email to Mailjet contacts' }),
+      body: JSON.stringify({ message: 'Error adding email to Mailjet contacts' })
     };
   }
 };
