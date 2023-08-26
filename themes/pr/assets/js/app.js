a57131762e3b7689dff1d257039f9b2e96da9c0d
proteinreport <contact@proteinreport.org>
2023-08-26 22:23:56 +0200

    testing

--- themes/pr/assets/js/app.js
+++ themes/pr/assets/js/app.js
@@ -32,36 +32,36 @@ window.onscroll = function() {
 // submit newsletter form without redirect
 // adapted from
 // https://css-tricks.com/using-netlify-forms-and-netlify-functions-to-build-an-email-sign-up-widget/
-const processForm = form => {
-  const formData = new FormData(form);
-  const email = formData.get('email'); // Assuming your input field has name="email"
+// const processForm = form => {
+//   const formData = new FormData(form);
+//   const email = formData.get('email'); // Assuming your input field has name="email"
 
-  if (!email) {
-    form.innerHTML = `<div class="form--error">Please enter your email address.</div>`;
-    return;
-  }
+//   if (!email) {
+//     form.innerHTML = `<div class="form--error">Please enter your email address.</div>`;
+//     return;
+//   }
 
-  fetch('/.netlify/functions/subscribeToMailerLite', {
-    method: 'POST',
-    body: new URLSearchParams({ email }), // Convert to URLSearchParams
-  })
-  .then(response => response.json().then(result => ({ status: response.status, result })))
-  .then(({ status, result }) => {
-    if (status === 200 || status === 201 || status === 202 || status === 204) {
-      form.innerHTML = `<div class="form--success">Almost there! Check your inbox for a confirmation e-mail. If you had previously unsubscribed, you've now been resubscribed.</div>`;
-    } else {
-      form.innerHTML = `<div class="form--error">Error: ${result.message}</div>`;
-    }
-  })
-  .catch(error => {
-    form.innerHTML = `<div class="form--error">Error: ${error}</div>`;
-  });
-}
+//   fetch('/.netlify/functions/subscribeToMailerLite', {
+//     method: 'POST',
+//     body: new URLSearchParams({ email }), // Convert to URLSearchParams
+//   })
+//   .then(response => response.json().then(result => ({ status: response.status, result })))
+//   .then(({ status, result }) => {
+//     if (status === 200 || status === 201 || status === 202 || status === 204) {
+//       form.innerHTML = `<div class="form--success">Almost there! Check your inbox for a confirmation e-mail. If you had previously unsubscribed, you've now been resubscribed.</div>`;
+//     } else {
+//       form.innerHTML = `<div class="form--error">Error: ${result.message}</div>`;
+//     }
+//   })
+//   .catch(error => {
+//     form.innerHTML = `<div class="form--error">Error: ${error}</div>`;
+//   });
+// }
 
-const emailForm = document.querySelector('.email-form');
-if (emailForm) {
-  emailForm.addEventListener('submit', e => {
-    e.preventDefault();
-    processForm(emailForm);
-  });
-}
+// const emailForm = document.querySelector('.email-form');
+// if (emailForm) {
+//   emailForm.addEventListener('submit', e => {
+//     e.preventDefault();
+//     processForm(emailForm);
+//   });
+// }
