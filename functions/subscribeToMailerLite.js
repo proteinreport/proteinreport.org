64a25a917c1ecb6f9e47e92863c3266725d80d82
proteinreport <contact@proteinreport.org>
2023-08-26 18:10:57 +0200

    ml testing

--- functions/subscribeToMailerLite.js
+++ functions/subscribeToMailerLite.js
@@ -1,47 +1,45 @@
-const axios = require('axios');
+const axios = require("axios");
 
 exports.handler = async (event) => {
+  const parsedData = new URLSearchParams(event.body);
+  const dataObj = {};
+  for (var pair of parsedData.entries()) {
+    dataObj[pair[0]] = pair[1];
+  }
+  console.log("DataObj: ", dataObj);
+  const email = dataObj.email;
+
+  // Get environment variables
+  const { ML_APIKEY, ML_GROUPID } = process.env;
+
+  // Create the contact in Mailjet
   try {
-    const parsedData = new URLSearchParams(event.body);
-    const dataObj = {};
-    for (var pair of parsedData.entries()) {
-      dataObj[pair[0]] = pair[1];
-    }
-
-    const email = dataObj.email;
-
-    // Get environment variables
-    const { MAILERLITE_PRODUCTION_API_KEY, MAILERLITE_PRODUCTION_NEWSLETTER_GROUP_ID } = process.env;
-
-    // Create the subscriber in MailerLite
     const response = await axios.post(
-      'https://connect.mailerlite.com/api/subscribers',
+      "https://connect.mailerlite.com/api/subscribers",
       {
-        email,
-        groups: [MAILERLITE_PRODUCTION_NEWSLETTER_GROUP_ID],
+        email: email,
+        groups: [ML_GROUPID],
       },
       {
         headers: {
           'Content-Type': 'application/json',
-          'Accept': 'application/json',
-          'Authorization': 'Bearer' + MAILERLITE_PRODUCTION_API_KEY,
+          Authorization: 'Bearer ' + ML_APIKEY,
         },
       }
     );
-
     return {
-      statusCode: response.status,
+      statusCode: 200,
       body: JSON.stringify({
-        message: 'Subscriber added to MailerLite successfully',
+        message: "Contact added to MailerLite successfully",
       }),
     };
   } catch (error) {
-    console.error('Error adding subscriber to MailerLite:', error);
+    console.error("Error adding contact to MailerLite:", error);
 
     return {
-      statusCode: error.response ? error.response.status : 500,
+      statusCode: 500,
       body: JSON.stringify({
-        message: 'Failed to add subscriber to MailerLite',
+        message: "Failed to add contact to MailjerLite",
       }),
     };
   }
