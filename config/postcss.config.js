e801958859663dbf211817e64f33514424074d70
proteinreport <contact@proteinreport.org>
2023-11-28 14:29:29 +0100

    Revert "upgrade Hyas and images and seo modules"
    This reverts commit 75f38ae62b5231121bba86ba29bdaa5ff0520aba.

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -7,9 +7,4 @@ const autoprefixer = require('autoprefixer');
 module.exports = {
   // eslint-disable-next-line no-process-env
 	plugins: [tailwind, ...(process.env.HUGO_ENVIRONMENT === 'production' ? [autoprefixer] : [])],
-  safelist: [
-    'img-fluid',
-    'lazyloaded',
-    'blur-up',
-  ],
 }
