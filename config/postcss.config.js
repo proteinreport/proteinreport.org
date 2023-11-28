75f38ae62b5231121bba86ba29bdaa5ff0520aba
proteinreport <contact@proteinreport.org>
2023-11-28 12:30:20 +0100

    upgrade Hyas and images and seo modules

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -7,4 +7,9 @@ const autoprefixer = require('autoprefixer');
 module.exports = {
   // eslint-disable-next-line no-process-env
 	plugins: [tailwind, ...(process.env.HUGO_ENVIRONMENT === 'production' ? [autoprefixer] : [])],
+  safelist: [
+    'img-fluid',
+    'lazyloaded',
+    'blur-up',
+  ],
 }
